dda2044f163339a5bfb182cad6641bd99cc4de36
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-10-26 00:48:09 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,10 @@ subprojects {
     apply(plugin = "kotlin-android")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
+
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
         // you can modify it to use other git hosting services, like gitlab
